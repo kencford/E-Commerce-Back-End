0a07ed8b981e547838267b17bc8092f48cc7b7a3
Ken Crawford <KenCrawford@Fastmail.com>
2021-12-13 19:46:36 -0500

    completed models for category, product, productTag, tag

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = require('./routes');
+
+// import sequelize connection
+//******************************* */
+//added by ken
+const sequelize = require('./config/connection');
+//******************************* */
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+
+// turn on connection to db and server
+//******************* */
+//added by ken
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+//******************** */
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
